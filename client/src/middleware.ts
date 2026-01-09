c910ca5dcce42f9ab552cbd016997964258b11fd
shomik-das <dashomik@gmail.com>
2026-01-09 23:48:27 +0600

    fix middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -5,11 +5,11 @@ export function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value
   const { pathname } = req.nextUrl
 
-  if (!token && pathname.startsWith("/dashboard/add-link")) {
+  if (!token && pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/auth", req.url))
   }
 
-  if (token && pathname === "/auth") {
+  if (token && pathname.startsWith("/auth")) {
     return NextResponse.redirect(new URL("/dashboard/add-link", req.url))
   }
 
